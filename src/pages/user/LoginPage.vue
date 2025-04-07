e303a96b41b7b15906aae7779a648f59dd26e8f3
JH Kim <rlawogus65@gmail.com>
2025-04-07 17:15:01 +0900

    feat: 회원 존재 여부를 통한 로그인 기능 구현

--- src/pages/user/LoginPage.vue
+++ src/pages/user/LoginPage.vue
@@ -4,12 +4,54 @@
     <div>
       <input type="text" placeholder="e-mail" v-model="email" required />
       <input type="password" placeholder="password" v-model="password" required />
-      <button>로그인</button>
-      <button>회원가입</button>
+      <button @click="loginHandler">로그인</button>
+      <button @click="goToSignUp">회원가입</button>
     </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, toRaw } from 'vue'
+import { useRouter } from 'vue-router'
+import axios from 'axios'
+
+const router = useRouter()
+
+const email = ref('')
+const password = ref('')
+const users = ref([])
+
+const getUsers = async () => {
+  try {
+    const response = await axios.get('/api/user')
+    users.value = response.data
+  } catch (error) {
+    console.error('오류 발생', error)
+  }
+}
+
+const loginHandler = async () => {
+  try {
+    await getUsers()
+
+    const existUser = users.value.find(
+      (user) => user.email === email.value && user.password === password.value,
+    )
+
+    if (existUser) {
+      console.log('로그인 성공,', toRaw(existUser))
+      router.push('/home')
+    } else {
+      alert('이메일 또는 비밀번호가 일치하지 않습니다.')
+    }
+  } catch (error) {
+    console.error('오류 발생: ', error)
+  }
+}
+
+const goToSignUp = () => {
+  router.push('/signup')
+}
+</script>
 
 <style scoped></style>
