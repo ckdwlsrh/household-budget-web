4dc872ee3a7730e27495d01a5e5b4c1b86a0970d
JH Kim <rlawogus65@gmail.com>
2025-04-07 16:54:48 +0900

    feat: 로그인 페이지 입력 폼 추가

--- src/pages/user/LoginPage.vue
+++ src/pages/user/LoginPage.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>LOGINPAGE</div>
+  <div>
+    <h3>LOGINPAGE</h3>
+    <div>
+      <input type="text" placeholder="e-mail" v-model="email" required />
+      <input type="password" placeholder="password" v-model="password" required />
+      <button>로그인</button>
+      <button>회원가입</button>
+    </div>
+  </div>
 </template>
 
 <script setup></script>
