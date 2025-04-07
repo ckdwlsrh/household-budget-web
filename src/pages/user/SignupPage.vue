3b0180c3529d99f258cb04cac42ee87a69c40093
JH Kim <rlawogus65@gmail.com>
2025-04-07 17:41:08 +0900

    feat: 회원가입 페이지 입력 폼 추가

--- src/pages/user/SignupPage.vue
+++ src/pages/user/SignupPage.vue
@@ -1,7 +1,28 @@
 <template>
-  <div></div>
+  <div>
+    <h3>SIGNUPPAGE</h3>
+    <div>
+      <input type="text" placeholder="name" />
+      <input type="email" placeholder="email" />
+      <input type="password" placeholder="password" />
+      <input type="password" placeholder="password" />
+      <button @click="goToLogin">뒤로가기</button>
+      <button @click="goToLogin">회원가입</button>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import axios from 'axios'
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const name = ref('')
+const email = ref('')
+const password = ref('')
+const users = ref([])
+</script>
 
 <style scoped></style>
