86153f0e300fa2435998475f88658b296194052e
jjang0308 <jang653838@naver.com>
2025-04-08 14:32:36 +0900

    Refator : RouterLink타입 변경 - 서버 테스트 결과 링크이동 불가하여 수정 - issue : #11

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <div>
+    <Header></Header>
+    <Nav></Nav>
     <router-view></router-view>
+    <Footer></Footer>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import Header from '@/components/Layouts/HeaderView.vue'
+import Footer from './components/Layouts/FooterView.vue'
+import Nav from './components/Layouts/NavView.vue'
+
+import { RouterView } from 'vue-router'
+</script>
 
 <style scoped></style>
--- src/components/Layouts/NavView.vue
+++ src/components/Layouts/NavView.vue
@@ -2,12 +2,17 @@
   <nav class="bg-gray-800 text-white">
     <div class="container mx-auto py-2">
       <ul class="flex space-x-4">
-        <li><a href="#" class="hover:text-blue-400">Home</a></li>
-        <li><a href="#" class="hover:text-blue-400">About</a></li>
-        <li><a href="#" class="hover:text-blue-400">Contact</a></li>
+        <RouterLink to="/home">대시보드</RouterLink>
+        <RouterLink to="/profileEdit">내 정보</RouterLink>
+        <RouterLink to="/budgetList">거래 내역</RouterLink>
+        <RouterLink to="/#">월별 재정</RouterLink>
       </ul>
     </div>
   </nav>
 </template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
 
 <style scoped></style>
