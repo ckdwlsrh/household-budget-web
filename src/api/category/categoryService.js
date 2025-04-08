b0575a9ad79358187e859b74d3ba3d9cf6984692
jjang0308 <jang653838@naver.com>
2025-04-08 11:02:05 +0900

    Feat : categoryService.js 추가 - category 조회 api 추가 - issue : #4

---
+++ src/api/category/categoryService.js
@@ -0,0 +1,22 @@
+import { handleError, handleResponse } from '../apiUtil'
+import apiClient from '../apiClient'
+
+const BASE = '/category'
+
+const getCategories = async () => {
+  try {
+    const response = await apiClient.get(BASE)
+    return handleResponse(response)
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+const getCategoryById = async (id) => {
+  try {
+    const response = await apiClient.get(BASE + `/${id}`)
+    return handleResponse(response)
+  } catch (error) {
+    handleError(error)
+  }
+}
