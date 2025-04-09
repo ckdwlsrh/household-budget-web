b2d4cbea16c3e6c0be323abb28601f93704eaa80
JH Kim <rlawogus65@gmail.com>
2025-04-09 17:51:46 +0900

    feat: 공지사항 기본 출력 기능 구현

--- src/pages/notices/NoticeListPage.vue
+++ src/pages/notices/NoticeListPage.vue
@@ -1,5 +1,37 @@
 <template>
-  <div>NoticeListPage</div>
+  <div class="container">
+    <div style="padding: 30px; margin-top: 30px">
+      <h1 style="text-align: center">공지사항</h1>
+    </div>
+    <!--  -->
+    <div>
+      <div v-for="notice in notices" :key="notice.id">
+        {{ notice.title }}
+      </div>
+    </div>
+    <!--  -->
+    <div>
+      <button>작성하기</button>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { getNotices } from '@/api/notices/noticeService'
+import { onMounted, ref } from 'vue'
+
+const notices = ref([])
+
+const featchNotices = async () => {
+  try {
+    const data = await getNotices()
+    notices.value = data
+  } catch (error) {
+    console.error('에러발생: ', error)
+  }
+}
+
+onMounted(() => {
+  featchNotices()
+})
+</script>
