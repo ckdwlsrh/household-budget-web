0863ad0781a376ec056ac7aaa5816436755d5840
jjang0308 <jang653838@naver.com>
2025-04-09 11:42:29 +0900

    Feat : navView, headerView 로직 추가 - navView에 레이아웃 디자인/ 기본 반응형디자인 추가/ 로그인 되있지 않으면 보이지 않게 수정 - issue : #11

--- src/components/Layouts/NavView.vue
+++ src/components/Layouts/NavView.vue
@@ -1,18 +1,65 @@
 <template>
   <nav class="bg-gray-800 text-white">
-    <div class="container mx-auto py-2">
-      <ul class="flex space-x-4">
-        <RouterLink :to="{ name: 'homePage' }">대시보드</RouterLink>
-        <RouterLink :to="{ name: 'profileEditPage' }">내 정보</RouterLink>
-        <RouterLink :to="{ name: 'budgetListPage' }">거래 내역</RouterLink>
-        <RouterLink :to="{ name: 'homePage' }">월별 재정</RouterLink>
-      </ul>
+    <div class="container">
+      <div class="row">
+        <template v-if="isLoggedIn">
+          <ul class="nav nav-pills nav-justified">
+            <div class="nav-item">
+              <RouterLink
+                :to="{ name: 'homePage' }"
+                class="btn btn-link nav-link text-black custom-border"
+                >대시보드</RouterLink
+              >
+            </div>
+            <div class="nav-item">
+              <RouterLink
+                :to="{ name: 'profileEditPage' }"
+                class="btn btn-link nav-link text-black custom-border"
+                >내 정보</RouterLink
+              >
+            </div>
+            <div class="nav-item">
+              <RouterLink
+                :to="{ name: 'budgetListPage' }"
+                class="btn btn-link nav-link text-black custom-border"
+                >거래 내역</RouterLink
+              >
+            </div>
+            <div class="nav-item">
+              <RouterLink
+                :to="{ name: 'homePage' }"
+                class="btn btn-link nav-link text-black custom-border"
+                >월별 재정</RouterLink
+              >
+            </div>
+          </ul>
+        </template>
+      </div>
     </div>
   </nav>
 </template>
 
 <script setup>
 import { RouterLink } from 'vue-router'
+import { onMounted } from 'vue'
+import { ref } from 'vue'
+
+const isLoggedIn = ref(false)
+
+onMounted(() => {
+  const storedUser = localStorage.getItem('loggedUser')
+  if (storedUser) {
+    isLoggedIn.value = true
+  }
+})
 </script>
 
-<style scoped></style>
+<style scoped>
+.nav-link:hover {
+  border-color: #ffcc00;
+}
+.custom-border {
+  border: 2px solid #dddddd; /* 원하는 색상으로 변경 */
+  border-radius: 0.25rem; /* 테두리 둥글기 조정 */
+}
+</style>
