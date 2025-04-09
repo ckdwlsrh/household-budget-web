ae7a3e26146946e290d728d3268dfe58c950aaaf
JH Kim <rlawogus65@gmail.com>
2025-04-09 14:20:19 +0900

    feat: router/index.js 로그인 했는지 검증 이후 페이지 접속

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import ProfileEditPage from '@/pages/user/ProfileEditPage.vue'
 import BudgetListPage from '@/pages/budgets/BudgetListPage.vue'
 import BudgetDetailPage from '@/pages/budgets/BudgetDetailPage.vue'
 import UpdateBudgetDetailPage from '@/pages/budgets/UpdateBudgetDetailPage.vue'
+// import { useUserStore } from '@/stores/userStore'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -48,5 +49,24 @@ const router = createRouter({
     },
   ],
 })
+
+// 로그인 안되있는데 다른 페이지 접속시 '/login' 으로 이동
+// 로그인이 되었는데 '/login' '/signup' 접속시 '/' 으로 이동
+// router.beforeEach((to, from, next) => {
+//   const userStore = useUserStore()
+//   const isLoggedIn = !!userStore.loggedUser
+
+//   const publicPages = ['/login', '/signup']
+
+//   if (!publicPages.includes(to.path) && !isLoggedIn) {
+//     return next('/login')
+//   }
+
+//   if (isLoggedIn && (to.path === '/login' || to.path === '/signup')) {
+//     return next('/')
+//   }
+
+//   next()
+// })
 
 export default router
