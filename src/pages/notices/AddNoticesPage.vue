e58283d79e3b6ccac9d0adefe9c0841a8b7056dc
JH Kim <rlawogus65@gmail.com>
2025-04-09 17:27:03 +0900

    refactor: 스타일 수정

--- src/pages/notices/AddNoticesPage.vue
+++ src/pages/notices/AddNoticesPage.vue
@@ -1,30 +1,32 @@
 <template>
   <div class="container">
     <div style="padding: 30px; margin-top: 30px">
-      <h1 style="text-align: center">공지사항 작성페이지</h1>
+      <h1 style="text-align: center">공지사항 작성</h1>
     </div>
     <div class="form-group">
-      <label>제목</label>
+      <label><p class="fs-3">제목</p></label>
       <input
         type="text"
         class="form-control"
         placeholder="제목을 입력해주세요"
         required
         v-model="title"
+        style="height: 50px; font-size: 20px"
       />
     </div>
     <br />
     <div class="form-group">
-      <label>내용</label>
+      <label><p class="fs-3">내용</p></label>
       <textarea
         type="text"
         class="form-control"
         placeholder="내용을 입력해주세요"
         required
         v-model="desc"
+        style="font-size: 20px"
       />
     </div>
-    <div>
+    <div class="button-group">
       <button class="btn btn-outline-dark" @click="goToNoticesList">목록보기</button>
       <button class="btn btn-outline-dark" @click="addNotice">추가하기</button>
     </div>
@@ -64,5 +66,12 @@ const goToNoticesList = () => {
 textarea {
   resize: none;
   height: 400px;
+}
+
+.button-group {
+  display: flex;
+  justify-content: flex-end;
+  gap: 15px;
+  margin-top: 20px;
 }
 </style>
