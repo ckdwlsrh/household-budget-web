97082f8857422afa837cbcf7306ebd59cc1ba104
JH Kim <rlawogus65@gmail.com>
2025-04-10 14:51:04 +0900

    feat: 헤더 이모티콘 추가

--- src/components/Layouts/HeaderView.vue
+++ src/components/Layouts/HeaderView.vue
@@ -1,8 +1,11 @@
 <template>
   <header class="container-fluid p-5" style="background-color: #fcfae9; color: black">
-    <div class="d-flex justify-content-between">
-      <span class="h2 fw-bold text-shadow"> 머니 매니저</span>
-      <button @click="logOut" class="btn btn-outline-dark text-end">로그아웃</button>
+    <div class="d-flex justify-content-between align-items-center">
+      <div class="d-flex align-items-center gap-1">
+        <img src="../../assets/img/money.png" width="50" height="50" />
+        <span class="h2 fw-bold text-shadow mb-0">머니 매니저</span>
+      </div>
+      <button @click="logOut" class="btn btn-outline-dark">로그아웃</button>
     </div>
   </header>
 </template>
